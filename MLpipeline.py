0f93e8f59620735231e9f99d0fbf66f784280a3b
Roméo Nazaret <47088975+romeodelyon69@users.noreply.github.com>
2025-11-05 10:59:10 +0100

    Add files via upload

---
+++ MLpipeline.py
@@ -0,0 +1,47 @@
+import torch 
+import MlTools as mlt
+
+model = mlt.EyeMLP()
+print(model)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+model.to(device)
+
+X_train, X_test, y_train, y_test = mlt.load_data("face_data1761048351.csv")
+print("Training data shape:", X_train.shape, y_train.shape)
+print("Testing data shape:", X_test.shape, y_test.shape)
+
+# Convert data to PyTorch tensors
+X_train = torch.from_numpy(X_train).float().to(device)
+X_test = torch.from_numpy(X_test).float().to(device)
+y_train = torch.from_numpy(y_train).float().to(device)
+y_test = torch.from_numpy(y_test).float().to(device)
+
+# Define loss function and optimizer and MAE loss
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0005, weight_decay=0.00001)
+
+#loss du predictor constant = 0.5/0.5 est de 0.0833 pour la MSE et de 0.25 pour la MAE
+# Training loop
+num_epochs = 12500
+for epoch in range(num_epochs):
+    model.train()
+    optimizer.zero_grad()
+    outputs = model(X_train)
+    loss = criterion(outputs, y_train)
+    loss.backward()
+    optimizer.step()
+    
+    if (epoch+1) % 1000 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+# Evaluate the model
+model.eval()
+with torch.no_grad():
+    test_outputs = model(X_test)
+    test_loss = criterion(test_outputs, y_test)
+    print(f'Test Loss: {test_loss.item():.4f}')
+
+    
+# Save the trained model
+torch.save(model.state_dict(), 'eye_mlp_model.pth')
---
+++ test.py
@@ -0,0 +1,31 @@
+import cv2
+import mediapipe as mp
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(refine_landmarks=True)
+mp_draw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, frame = cap.read()
+    if not success:
+        break
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(frame_rgb)
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            mp_draw.draw_landmarks(
+                frame, face_landmarks, mp_face_mesh.FACEMESH_TESSELATION,
+                mp_draw.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1)
+            )
+
+    cv2.imshow("Eye Tracker", frame)
+    
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
